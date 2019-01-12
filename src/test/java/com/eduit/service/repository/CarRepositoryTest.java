d2a3d55f2419a3c0c9eb8aadb7a69999ac59986a
Your Name <you@example.com>
2019-01-12 13:46:38 -0300

    wip

--- src/test/java/com/eduit/service/repository/CarRepositoryTest.java
+++ src/test/java/com/eduit/service/repository/CarRepositoryTest.java
@@ -1,14 +1,14 @@
 package com.eduit.service.repository;
 
 
-import com.eduit.service.ServiceApplication;
 import com.eduit.service.model.Car;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -19,17 +19,36 @@ public class CarRepositoryTest {
 
     @Autowired
     private CarRepository carRepository;
+    private Optional<Car> carOpc;
+
+    @Before
+    public void init(){
+        Car car = new Car("fiat","palio",80000D);
+        carRepository.save(car);
+    }
+
+    @After
+    public void after(){
+        carRepository.deleteAll();
+    }
 
     @Test
-    public void aaaaaaa(){
+    public void mustFindCarWithIdOne(){
+        givenCarRepository();
+        whenFindCarWithIdOne();
+        thenShouldFindCarWithIdOne();
+    }
 
-        Optional<Car> carOpc = carRepository.findById(1L);
+    private void thenShouldFindCarWithIdOne() {
+        Assert.assertEquals(true, carOpc.isPresent());
+    }
 
-        Assert.assertEquals(true,carOpc.isPresent());
-
-
+    private void whenFindCarWithIdOne() {
+        carOpc = carRepository.findById(1L);
     }
 
 
-
+    public CarRepository givenCarRepository() {
+        return carRepository;
+    }
 }
